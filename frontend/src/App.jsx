9df4a2ffb1c364377e1069c105f5144577fb86c1
HottBraz <joao.hott@mail.uft.edu.br>
2025-09-20 13:35:12 -0300

    Criação da pagina de Descrição do Evento

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Register from "./pages/Register"
 import Home from "./pages/Home"
 import NotFound from "./pages/NotFound"
 import ProtectedRoute from "./components/ProtectedRoute"
+import EventDescription from "./pages/EventDescription"
 
 function Logout() {
   localStorage.clear()
@@ -27,10 +28,11 @@ function App() {
             </ProtectedRoute>
           }
         />
-        <Route path="/login" element={<Login />} />
-        <Route path="/logout" element={<Logout />} />
-        <Route path="/register" element={<RegisterAndLogout />} />
-        <Route path="*" element={<NotFound />}></Route>
+  <Route path="/login" element={<Login />} />
+  <Route path="/logout" element={<Logout />} />
+  <Route path="/register" element={<RegisterAndLogout />} />
+  <Route path="/evento" element={<EventDescription />} />
+  <Route path="*" element={<NotFound />}></Route>
       </Routes>
     </BrowserRouter>
   )
