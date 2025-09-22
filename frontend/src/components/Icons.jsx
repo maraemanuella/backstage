be5bf636ca2c5fd9a0a0e9d013b4aeccae748dc7
HottBraz <joao.hott@mail.uft.edu.br>
2025-09-21 23:55:19 -0300

    Correções apontadas na issues

---
+++ frontend/src/components/Icons.jsx
@@ -0,0 +1,3 @@
+import { FaMapMarkerAlt, FaClock, FaCalendarAlt, FaUser, FaStar, FaInstagram, FaFacebook, FaLinkedin, FaArrowLeft, FaUsers, FaCheckCircle, FaShareAlt } from "react-icons/fa";
+
+export { FaMapMarkerAlt, FaClock, FaCalendarAlt, FaUser, FaStar, FaInstagram, FaFacebook, FaLinkedin, FaArrowLeft, FaUsers, FaCheckCircle, FaShareAlt };
